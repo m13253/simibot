8fd5df311166660593a75f4e3514c8619a1d37a1
Star Brilliant <m13253@hotmail.com>
2012-08-09 16:26:26 +0800

    Initial commit

--- simibot.py
+++ simibot.py
@@ -41,7 +41,11 @@ while 1:
             elif sline[1]=="PRIVMSG":
                 rnick=sline[0][1:].split("!")[0]
                 if line.find(" PRIVMSG %s :" % NICK)!=1 then:
-                    s.send("PRIVMSG %s :%s: 我不接受私信哦，在聊天室里面用“%s: ”开头就可以联系我。\r\n" % (rnick, rnick, NICK))
+                    if line.split(" PRIVMSG %s :" % NICK)[1]=="Get out of this channel!": # A small hack
+                        s.send("QUIT\r\n")
+                        exit()
+                    else:
+                        s.send("PRIVMSG %s :%s: 我不接受私信哦，在聊天室里面用“%s: ”开头就可以联系我。\r\n" % (rnick, rnick, NICK))
                 else:
                     if line.split(" PRIVMSG %s :" % CHAN)[1].startswith("%s:" % NICK):
                         resp=urllib.urlopen("http://www.simsimi.com/func/req?%s" % urllib.urlencode({"lc": "zh", "msg": line.split(" PRIVMSG %s :%s:" % (CHAN, NICK))[1].lstrip()})).read()
@@ -52,6 +56,6 @@ while 1:
                         time.sleep(random.randomm()*2)
                         s.send("PRIVMSG %s :%s: %s\r\n" % (CHAN, rnick, resp))
         except:
-            s.send("PRIVMSG %s :貌似我出了故障，可能会运行不稳定，请重新启动。\r\n" % CHAN)
+            s.send("PRIVMSG %s :%s 出现了一点小故障，正在努力恢复工作。\r\n" % (CHAN, NICK))
 
 # vim: et ft=python sts=4 sw=4 ts=4
